e6e5330ac98af9b437194c41993df3bdb3c0b4bc
tokkss <olayinkanafisah@gmail.com>
2022-07-13 14:31:55 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,6 @@ void more_numbers(void)
 	char n, c;
 	int i = 0;
 
-
 	while (i < 10)
 	{
 		for (n = 0; n <= 14; n++)
@@ -18,18 +17,17 @@ void more_numbers(void)
 			c = n;
 			if (n > 9)
 			{
-	
+
 				_putchar('1');
 				c = n % 10;
 			
 			}
-	
+
 			_putchar('0' + c);
 		}
 
 		_putchar('\n');
 		i++;
 	}
-
 
 }
